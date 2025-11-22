5fb740a152a94aaaacc5f615ce8c90f23a4a5889
Seema Kumari <seema.kumari@orangetech.ai>
2025-11-22 22:41:14 +0530

    initial commit

--- src/main/java/com/alive/gateway/DesktopLauncher.java
+++ src/main/java/com/alive/gateway/DesktopLauncher.java
@@ -6,14 +6,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 public class DesktopLauncher {
     public static void main(String[] args) {
+        System.out.println(">>> DesktopLauncher main started <<<");
         //Start Spring Boot in Background thread (Same JVM)
         new Thread(() -> {
+            System.out.println(">>> Starting Spring Boot <<<");
             new SpringApplicationBuilder(GatewayApplication.class)
                     .logStartupInfo(false)
                     .run(args);
         }).start();
 
         //Then Start JavaFX UI
+        System.out.println(">>> Launching JavaFX <<<");
         Application.launch(App.class, args);
     }
 }
